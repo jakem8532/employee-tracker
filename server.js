02707d16aeb9f0ed4eb8c864c961f06509c2c0f5
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-07-30 15:03:30 -0400

    Final commit

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ function start() {
     inquirer.prompt([
         {
             type: 'list',
-            name: 'choice',
+            name: 'action',
             message: 'Select an action',
             choices: [
                 'View Employees',
                 'Add Employee',
                 'View Roles',
-                'Add role',
+                'Add Role',
                 'View Departments',
                 'Add Department',
                 'Quit'
             ]
         }
     ]).then(answer => {
-        switch(answer.choice) {
+        switch(answer.action) {
             case 'View Employees':
                 viewEmployees()
                 break;
@@ -56,20 +56,253 @@ function start() {
 }
 
 function viewEmployees() {
-    const sql = `SELECT * FROM employee.*, role.title
+    const sql = `SELECT employee.*, role.title
                 AS role_title
                 FROM employee
-                LEFT JOIN role
-                ON employee.role_id = role.title`
+                JOIN role
+                ON role.id = employee.role_id
+                `
     
     db.query(sql, (err, rows) => {
         if (err) throw err
         console.log('Viewing All Employees')
         console.table(rows)
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'select action',
+                choices: [
+                    'Menu',
+                    'Quit'
+                ]
+            }
+        ]).then(answer => {
+            switch(answer.action) {
+                case 'Menu':
+                    start()
+                    break;
+                
+                case 'Quit':
+                    quit()
+                    break;
+            }
+        })
+    })
+}
+
+function addEmployee() {
+    console.log('Fill in the following info about the new employee')
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the employees first name',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'Enter employees last name',
+            name: 'lastName',
+        },
+        {
+            type: 'input',
+            message: 'Enter employee role ID number',
+            name: 'roleId'
+        },
+        {
+            type: 'input',
+            message: 'Enter employee manager ID',
+            name: 'managerId'
+        }
+    ]).then(answer => {
+        const sql = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)`
+        const params = [answer.firstName, answer.lastName, answer.roleId, answer.managerId]
+
+        db.query(sql, params, (err, results) => {
+            if (err) throw err
+            console.table(results)
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'select action',
+                    choices: [
+                        'Menu',
+                        'Quit'
+                    ]
+                }
+            ]).then(answer => {
+                switch(answer.action) {
+                    case 'Menu':
+                        start()
+                        break;
+                    
+                    case 'Quit':
+                        quit()
+                        break;
+                }
+            })
+        })
+    })
+}
+
+function viewRoles() {
+    const sql = `SELECT * FROM role`
+
+    db.query(sql, (err, res) => {
+        if (err) throw err
+        console.log('Viewing all Roles')
+        console.table(res)
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'select action',
+                choices: [
+                    'Menu',
+                    'Quit'
+                ]
+            }
+        ]).then(answer => {
+            switch(answer.action) {
+                case 'Menu':
+                    start()
+                    break;
+                
+                case 'Quit':
+                    quit()
+                    break;
+            }
+        })
+    })
+}
+
+function addRole() {
+    console.log('Fill in the following info about the new role')
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the role title',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Enter the roles salary',
+            name: 'salary',
+        },
+        {
+            type: 'input',
+            message: 'Enter the department id number',
+            name: 'departmentId'
+        }
+    ]).then(answer => {
+        const sql = `INSERT INTO role(title, salary, department_id) VALUES(?, ?, ?)`
+        const params = [answer.title, answer.salary, answer.departmentId]
+
+        db.query(sql, params, (err, results) => {
+            if (err) throw err
+            console.table(results)
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'select action',
+                    choices: [
+                        'Menu',
+                        'Quit'
+                    ]
+                }
+            ]).then(answer => {
+                switch(answer.action) {
+                    case 'Menu':
+                        start()
+                        break;
+                    
+                    case 'Quit':
+                        quit()
+                        break;
+                }
+            })
+        })
+    })
+}
+
+function viewDepartments() {
+    const sql = `SELECT * FROM departments`
+
+    db.query(sql, (err, res) => {
+        if (err) throw err
+        console.log('Viewing all Departments')
+        console.table(res)
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'select action',
+                choices: [
+                    'Menu',
+                    'Quit'
+                ]
+            }
+        ]).then(answer => {
+            switch(answer.action) {
+                case 'Menu':
+                    start()
+                    break;
+                
+                case 'Quit':
+                    quit()
+                    break;
+            }
+        })
+    })
+}
+
+function addDepartment() {
+    console.log('Fill out the following information about the department')
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the new department',
+            name: 'departmentName'
+        }
+    ]).then(answer => {
+        const sql = `INSERT INTO departments(department_name) VALUES(?)`
+        const params = [answer.departmentName]
+
+        db.query(sql, params, (err, results) => {
+            if (err) throw err
+            console.table(results)
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'select action',
+                    choices: [
+                        'Menu',
+                        'Quit'
+                    ]
+                }
+            ]).then(answer => {
+                switch(answer.action) {
+                    case 'Menu':
+                        start()
+                        break;
+                    
+                    case 'Quit':
+                        quit()
+                        break;
+                }
+            })
+        })
     })
 }
 
 
+function quit() {
+    console.log('Bye!')
+    process.exit()
+}
 
 
 db.connect(() => {
